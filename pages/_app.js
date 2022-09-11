5ea52ffa9cc0c72f5e9980fe569343301882640b
CrisRondaS <cristian.ronda@shockoe.com>
2022-09-10 19:42:42 -0500

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,32 @@
 import { ToastContainer } from 'react-toastify';
 import '@styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
+import Head from 'next/head';
+import { useEffect } from 'react';
 
 const Noop = ({ children }) => <>{children}</>;
 function MyApp({ Component, pageProps }) {
     const Layout = Component.Layout ?? Noop;
+    useEffect(() => {
+        (function titleScroller(text) {
+            document.title = text;
+            console.log(text);
+            setTimeout(function () {
+                titleScroller(text.substring(1) + text.substring(0, 1));
+            }, 500);
+        })(' 🚀Marketplace Ropsten Course🚀 ');
+    }, []);
+
     return (
-        <Layout>
-            <Component {...pageProps} />
-            <ToastContainer />
-        </Layout>
+        <>
+            <Head>
+                <title> </title>
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+                <ToastContainer />
+            </Layout>
+        </>
     );
 }
 
